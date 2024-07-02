9be629108a88a28759e82007fa6d14481bb8751a
Albert Mehring <166022423+almehr1@users.noreply.github.com>
2024-07-02 12:25:00 -0300

    Add files via upload
    Simple software example

---
+++ Hotel.py
@@ -0,0 +1,13 @@
+einzelzimmer = 80€
+doppelzimmer = 100€
+
+eingabe = date(input("Gib ein Datum ein in diesem Jahr"))
+
+while eingabe in range(01.01.2020,31.12.2020)
+    try:
+        print("Zimmerkategorie:")
+        eingabe -1=
+            if
+    elif ValueError:
+        print(eingabe)
+    else:
